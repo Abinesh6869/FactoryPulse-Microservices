b329f1ec5b97644a9183ccdabaa3cce813f491ca
Abinesh <abinesh.g@cognizant.com>
2026-05-05 21:05:09 +0530

    feat: Eureka service registry

---
+++ src/test/java/org/cts/fp_registry/FpRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cts.fp_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

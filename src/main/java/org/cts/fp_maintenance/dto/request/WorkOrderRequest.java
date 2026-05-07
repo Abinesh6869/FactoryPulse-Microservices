45ee6e6e1308f1a9e79d7f25ed7531070fda70e0
Abinesh <abinesh.g@cognizant.com>
2026-05-07 21:47:44 +0530

    feat: update WorkOrderRequest and application properties

--- src/main/java/org/cts/fp_maintenance/dto/request/WorkOrderRequest.java
+++ src/main/java/org/cts/fp_maintenance/dto/request/WorkOrderRequest.java
@@ -1,10 +1,12 @@
 package org.cts.fp_maintenance.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WorkOrderRequest {
 
     @NotNull(message = "Machine ID is required")
